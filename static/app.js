cfbeac33d3dbc2a3f7e7585cb001fc7ead31ea1b
Jassi Bacha <klikster@gmail.com>
2023-04-05 16:25:25 -0700

    swap to async

--- static/app.js
+++ static/app.js
@@ -1,41 +1,25 @@
-const $form = $('#guess-form');
-const $guess = $('#word');
+async () => {
+    const $form = $('#guess-form');
+    const $guess = $('#word');
 
-// add an empty set called words
+    // add an empty set called words
 
-// Form submission listener
-$form.on('submit', (event) => {
-    event.preventDefault(); // Prevent page refresh
+    // Form submission listener
+    $form.on('submit', async (event) => {
+        event.preventDefault(); // Prevent page refresh
 
-    const word = $guess.val(); // Get the guessed word
-    if (!word) return;
+        const word = $guess.val(); // Get the guessed word
+        if (!word) return; // if no word stop
 
-    // check if words already has the word const, if so show an error message
+        // check if words already has the word const, if so show an error message
 
-    const resp = await axios.get('/check-word', { params: { word: word }});
+        const resp = await axios.get('/check-word', { params: { word: word } });
 
-    print(resp) // find the result in the data
+        print(resp); // find the result in the data
 
-    // if goes here
-    // 'not-word'
-    // 'not-on-board'
-    // else add the word
-
-    // axios
-    //     .post('/check-word', { word })
-    //     .then((response) => {
-    //         const isValid = response.data.isValid;
-    //         // Update the page based on whtether word is valid or not
-    //         if (isValid) {
-    //             console.log('word is valid');
-    //             return True;
-    //         } else {
-    //             // Do something else
-    //             console.log('word is NOT valid');
-    //             return False;
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         console.error(error);
-    //     });
-});
+        // if goes here
+        // 'not-word'
+        // 'not-on-board'
+        // else add the word
+    });
+};
